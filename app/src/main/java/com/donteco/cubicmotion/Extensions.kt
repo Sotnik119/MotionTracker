9bcc178579936402b0f840f670eea7080ae4d4ac
Oleg Sotnik <sotnik119@gmail.com>
2019-10-25 13:01:01 +0300

    some design

--- app/src/main/java/com/donteco/cubicmotion/Extensions.kt
+++ app/src/main/java/com/donteco/cubicmotion/Extensions.kt
@@ -6,11 +6,14 @@ import com.google.gson.Gson
 
 fun AppCompatActivity.hideSystemUI() {
     val decorView: View = window.decorView
+    decorView.setOnSystemUiVisibilityChangeListener {
+        hideSystemUI()
+    }
     decorView.systemUiVisibility = (
             View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                     or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                     or View.SYSTEM_UI_FLAG_FULLSCREEN)
 
